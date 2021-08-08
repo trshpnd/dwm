c8c7b5b3df18b2dd14e47dddbdc184ea4fc17a99
trshpnd <73907087+trshpnd@users.noreply.github.com>
2021-08-08 02:06:32 -0300

    Add files via upload

---
+++ patch/include.c
@@ -0,0 +1,23 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_powerline_status.c"
+#include "bar_powerline_tags.c"
+#include "bar_status.c"
+#include "bar_statuscolors.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+
+/* Other patches */
+#include "cool_autostart.c"
+#include "cyclelayouts.c"
+#include "focusdir.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_powerline_status.h"
+#include "bar_powerline_tags.h"
+#include "bar_status.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "cool_autostart.h"
+#include "cyclelayouts.h"
+#include "focusdir.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
